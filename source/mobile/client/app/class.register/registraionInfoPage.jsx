43725f30560bd74932a0aa8dae4d28badff8adc9
ennea8 <ennea8@gmail.com>
2015-09-15 11:18:43 -0700

    1.Add shoppingCart collection 2.change classes doc structure  and add seed data 3.Add classregister module

---
+++ source/mobile/client/app/class.register/registraionInfoPage.jsx
@@ -0,0 +1,32 @@
+/**
+ * Created on 9/14/15.
+ */
+
+Cal.CRRegistraionInfoPage = React.createClass({
+
+    mixins: [ReactMeteorData],
+    getMeteorData() {
+        return {}
+    },
+
+    render() {
+        return <div >
+            <RC.Card title="registraRoninfo">
+                <p class="padding">
+                    sfas
+                    sfasf
+                    fasf
+
+                </p>
+            </RC.Card>
+
+            <RC.URL href="/SelectClass">
+                <RC.Button name="button" theme="full"  buttonColor="brand">
+                    Continue
+                </RC.Button>
+            </RC.URL>
+
+
+        </div>
+    }
+})
--- source/mobile/server/seeds/classes.js
+++ source/mobile/server/seeds/classes.js
@@ -1,18 +1,25 @@
 Meteor.startup(function () {
     DB.Classes.remove()
+
     if (DB.Classes.find({}).count() === 0) {
 
         //////////////////////////////////
         //构造session1的class数据
 
         DB.Classes.insert({
-            _id: 'testSession1' + 'class1',
+            _id:  'class1',
             sessionId: 'testSession1',
             name: 'testSession1' + 'class1',
+
+            level: 'level-1',
+            day:1,
+            startTime:App.time2num('9:00'),
+            endTime:App.time2num('11:00'),
+
             startDate: new Date(),
             endDate: new Date(+new Date() + (1000 * 60 * 60 * 24 * 90  )),//90天后结束
             frequency: "2/week",
-            level: 'level-1',
+
             type: 'ss',
             availableSeats: 10,
             price: 100,
@@ -20,9 +27,15 @@ Meteor.startup(function () {
         });
 
         DB.Classes.insert({
-            _id: 'testSession1' + 'class2',
+            _id: 'class2',
             sessionId: 'testSession1',
             name: 'testSession1' + 'class2',
+
+            level: 'level-1',
+            day:1,
+            startTime:App.time2num('14:00'),
+            endTime:App.time2num('16:00'),
+
             startDate: new Date(),
             endDate: new Date(+new Date() + (1000 * 60 * 60 * 24 * 90  )),//90天后结束
             frequency: "2/week",
@@ -34,9 +47,15 @@ Meteor.startup(function () {
         });
 
         DB.Classes.insert({
-            _id: 'testSession1' + 'class3',
+            _id:  'class3',
             sessionId: 'testSession1',
             name: 'testSession1' + 'class3',
+
+            level: 'level-1',
+            day:1,
+            startTime:App.time2num('15:00'),
+            endTime:App.time2num('17:00'),
+
             startDate: new Date(),
             endDate: new Date(+new Date() + (1000 * 60 * 60 * 24 * 90  )),//90天后结束
             frequency: "2/week",
@@ -50,20 +69,69 @@ Meteor.startup(function () {
 
         ////////////////////////////////////////////
         //构造session2的class数据
+        //session2 和session的class一样时才可以bookTheSameTime
 
         DB.Classes.insert({
-            _id: 'testSession2'+'class1',
+            _id:  'testSession2class1',
             sessionId: 'testSession2',
-            name: 'testSession2'+'class1',
+            name: 'testSession2' + 'class1',
+
+            level: 'level-1',
+            day:1,
+            startTime:App.time2num('9:00'),
+            endTime:App.time2num('11:00'),
+
             startDate: new Date(),
-            endDate: new Date(+new Date() + (1000 * 60 * 60 * 24 * 90 )),
+            endDate: new Date(+new Date() + (1000 * 60 * 60 * 24 * 90  )),//90天后结束
+            frequency: "2/week",
+
+            type: 'ss',
+            availableSeats: 10,
+            price: 100,
+            students: ['swimmer1']   //swimmer1已选‘class1’
+        });
+
+        DB.Classes.insert({
+            _id: 'testSession2class2',
+            sessionId: 'testSession2',
+            name: 'testSession2' + 'class2',
+
+            level: 'level-1',
+            day:1,
+            startTime:App.time2num('14:00'),
+            endTime:App.time2num('16:00'),
+
+            startDate: new Date(),
+            endDate: new Date(+new Date() + (1000 * 60 * 60 * 24 * 90  )),//90天后结束
             frequency: "2/week",
             level: 'level-1',
             type: 'ss',
             availableSeats: 10,
             price: 100,
-            students: []
+            students: ['swimmer1']
         });
+
+        DB.Classes.insert({
+            _id:  'testSession2class3',
+            sessionId: 'testSession2',
+            name: 'testSession2' + 'class3',
+
+            level: 'level-1',
+            day:1,
+            startTime:App.time2num('15:00'),
+            endTime:App.time2num('17:00'),
+
+            startDate: new Date(),
+            endDate: new Date(+new Date() + (1000 * 60 * 60 * 24 * 90  )),//90天后结束
+            frequency: "2/week",
+            level: 'level-1',
+            type: 'ss',
+            availableSeats: 10,
+            price: 100,
+            students: []   //swimmer1已选‘class1’
+        });
+
+
 
 
     }
