4386208d157480c654bfb1d008260e7d18214f5a
ivanrex <ivanrex@126.com>
2015-10-09 12:04:31 -0700

    Implementation, sending verification email

--- source/mobile/server/seeds/auth.js
+++ source/mobile/server/seeds/auth.js
@@ -3,5 +3,6 @@ Meteor.startup(function () {
 	    url = url.replace('#/', '');
 	    return " To reset your password, simply click the link below:\n\n"
 	       + url;
-  };
+  	};
+  	process.env.MAIL_URL = 'smtp://postmaster@sandbox581a4fe7e1b54295be84ebc6ec8d93ea.mailgun.org:f242abd434c83aa5ca5e17d83dbd4bd1@smtp.mailgun.org:587/'
 });
