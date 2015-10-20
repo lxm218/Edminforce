ad4680bd5e48c332c2dd6093e8d4b0ddc49190c1
Jeffreyfan <ennea8@gmail.com>
2015-10-20 11:44:47 -0700

    add contact info

--- source/mobile/client/app/common/ContactInfoPage.jsx
+++ source/mobile/client/app/common/ContactInfoPage.jsx
@@ -12,36 +12,29 @@ Cal.ContactInfoPage = React.createClass({
     render() {
         return <div key={Math.random()}>
 
-            <RC.Card key={Math.random()} title="Contact Info">
+            <RC.Card key={Math.random()} title="Contact Us">
+
                 <div className="row">
                     <div className="col">
 
-                        <p>
+                        <center>
+                            ~~~~~ FREMONT ~~~~~<br/>
+                            Mon - Fri: 9am - 7pm<br/>
+                            Sat - Sun: 8am - 6pm<br/><br/>
 
-                            sfas
-                            sfasf
-                            fasf
-                            sfas
-                            sfasf
-                            fasfsfas
-                            sfasf
-                            fasfsfas
-                            sfasf
-                        </p>
+                            34075 Fremont Blvd<br/>
+                            Fremont, CA 94555<br/>
+                            (510) 790-7946<br/><br/>
 
-                        <p>
-                            fasfsfas
-                            sfasf
-                            fasfsfas
-                            sfasf
-                            fasfsfas
-                            sfasf
-                            fasfsfas
-                            sfasf
-                            fasfsfas
-                            sfasf
-                            fasf
-                        </p>
+                            ~~~~~ DUBLIN ~~~~~<br/>
+                            M, W, F: 12n - 8pm<br/>
+                            Tu, Th, Sa, Su: 9am - 5pm<br/><br/>
+
+                            6175 Dublin Blvd<br/>
+                            Dublin, CA 94568<br/>
+                            (925) 248-2989<br/>
+                        </center>
+
 
                     </div>
 
