3471cbc51f4299213bf7e35961e5f43c8ecbe7cf
Jeffreyfan <ennea8@gmail.com>
2015-10-21 15:34:21 -0700

    billing页显示多个未支付购物车（样式待优化）

--- source/mobile/client/app/class.edit/BillingUnfinished.jsx
+++ source/mobile/client/app/class.edit/BillingUnfinished.jsx
@@ -2,35 +2,59 @@ Cal.BillingUnfinished = React.createClass({
 
     render: function () {
 
+        /*
+         <RC.URL href="/classRegister/RegBillingPage">
+         <RC.Button name="button" type="submit"
+         onClick={this.formSubmit}
+         theme="full" buttonColor="brand">
+         Checkout
+         </RC.Button>
+
+         </RC.URL>
+
+
+        * */
+
+
         return <div>
 
             {
                 this.props.unfinishedBillings.map(function (cart) {
 
-                    return cart.items.map(function(item){
+                    return <div className="padding">
+                        <div>
+                        {
 
-                        return <div className="row">
-                            <div className="col">
-                                {item.swimmer &&item.swimmer.name }
-                            </div>
-                            <div className="col">
-                                {item.class1 &&item.class1.name }
+                            cart.items.map(function (item) {
 
-                            </div>
+                                return <div className="row" key={item.classId}>
+                                    <div className="col">
+                                        {item.swimmer && item.swimmer.name }
+                                    </div>
+                                    <div className="col">
+                                        {item.class1 && item.class1.name }
+
+                                    </div>
+                                </div>
+                            })
+
+                        }
                         </div>
-                    })
+
+                        <RC.Button name="button" type="submit"
+                                   onClick={this.formSubmit}
+                                   theme="full" buttonColor="brand">
+                            Checkout
+                        </RC.Button>
+
+
+                    </div>
+
+
                 })
             }
 
 
-            <RC.URL href="/classRegister/RegBillingPage">
-                <RC.Button name="button" type="submit"
-                           onClick={this.formSubmit}
-                           theme="full" buttonColor="brand">
-                    Checkout
-                </RC.Button>
-
-            </RC.URL>
 
 
 
