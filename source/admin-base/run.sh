69014acc4b79f07deb11f20b1ca352d94b9bdcca
Jacky <liyang@chinagate.com>
2016-01-27 12:11:47 +0800

    upload run shell

--- source/admin-base/run.sh
+++ source/admin-base/run.sh
@@ -3,20 +3,37 @@
 # export MONGO_URL="mongodb://jason:14dDSAdfK48XzAQpWoirL25j@c287.candidate.3.mongolayer.com:10287,candidate.3.mongolayer.com:10287,candidate.2.mongolayer.com:10325/framework?replicaSet=set-5580764dfc2b3f7b5f00001b"
 
 
-#echo PWD: $PWD
-#echo MONGO_URL: $MONGO_URL
-#
-#meteor --port 4000
 
-printf "params: %s %s %s\n " "$1" "$2" "$3"
+ENV1="PACKAGE_ROOT=$PWD/../_packages_"
+ENV2="RC_DIR=$PACKAGE_ROOT/ihealth-framework-ui/library"
+ENV3="PACKAGE_DIRS=$RC_DIR/both:$RC_DIR/mobile:$RC_DIR/webapp:$PACKAGE_ROOT/both:$PACKAGE_ROOT/module"
 
-export MONGO_URL="mongodb://127.0.0.1:27017/EdminForce"
-
-export PACKAGE_ROOT=$PWD/../_packages_
-export RC_DIR=$PACKAGE_ROOT/ihealth-framework-ui/library
-export PACKAGE_DIRS=$RC_DIR/both:$RC_DIR/mobile:$RC_DIR/webapp:$PACKAGE_ROOT/both:$PACKAGE_ROOT/module
-
-
+runLocalHost(){
+    export MONGO_URL="mongodb://127.0.0.1:27017/EdminForce"
+    export ENV1
+    export ENV2
+    export ENV3
+    meteor --port 8000
+}
 
 
-meteor --port 8000
+deploy(){
+    REMOTEURL="edminforce.meteor.com"
+    echo "start deploy to ${REMOTEURL}"
+    sudo ${ENV1} ${ENV2} ${ENV3} meteor deploy ${REMOTEURL}
+    echo "---- deploy success ----"
+}
+
+case "$1" in
+    deploy)
+        deploy
+        ;;
+    *)
+        runLocalHost
+        ;;
+
+esac
+
+
+
+
