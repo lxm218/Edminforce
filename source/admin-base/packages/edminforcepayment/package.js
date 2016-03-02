02eeb588f78480002c3b5a35564562a027a149f4
Yifan Wang <ivanrex@gmail.com>
2016-03-02 18:21:16 +0800

    capsule payment functions in admin-base

--- source/admin-base/packages/edminforcepayment/package.js
+++ source/admin-base/packages/edminforcepayment/package.js
@@ -2,23 +2,44 @@ Package.describe({
   name: 'ivanrex:edminforcepayment',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Server Method Package For Payment',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
+Package.onUse(function(api){
+    api.versionsFrom(['METEOR@1.0']);
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.use('ecmascript');
-  api.addFiles('edminforcepayment.js');
+    // The packages used for create this application
+    var packages = [
+
+    ];
+
+    api.use(packages);
+
+    api.addAssets([
+
+    ], 'client');
+
+    api.addFiles([
+
+    ], ["client", "server"]);
+
+    // Don't forget to add your jsx file
+    api.addFiles([
+        
+    ], ["client"]);
+
+    api.addFiles([
+      'Payment.Credit.jsx',
+      'Payment.ECheck.jsx'
+    ], ["server"]);
+
 });
 
+
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('ivanrex:edminforcepayment');
-  api.addFiles('edminforcepayment-tests.js');
+
 });
