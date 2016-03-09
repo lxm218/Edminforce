f7dfb55676fcb141410dcda40162c639128ef525
Jacky <liyang@chinagate.com>
2016-03-09 18:45:26 +0800

    update code

--- source/deploy/deploy.sh
+++ source/deploy/deploy.sh
@@ -5,7 +5,7 @@ sudo chmod 400 edminforce.pem;
 deployAdminBase(){
 
 	echo "---- connect to admin-base server ----";
-	CMD="cd Edminforce;git pull;cd source/admin-base;source run.sh aws;"
+	CMD="cd Edminforce;git reset --hard;git clean -d -fx;git pull;cd source/admin-base;source run.sh aws;"
 	#echo "${CMD}"
 	ssh -i "edminforce.pem" ubuntu@ec2-52-36-236-78.us-west-2.compute.amazonaws.com "${CMD}"
 	sudo chmod 777 edminforce.pem;
@@ -14,9 +14,9 @@ deployAdminBase(){
 deployMobileBase(){
 
 	echo "---- connect to mobile-base server ----";
-	CMD="cd Edminforce;git pull;cd source/mobile-base;source aws.sh;"
+	CMD="cd Edminforce;git reset --hard;git clean -d -fx;git pull;cd source/mobile-base;source aws.sh;"
 	echo "${CMD}"
-	ssh -i "edminforce.pem" ubuntu@ec2-52-36-230-108.us-west-2.compute.amazonaws.com "${CMD}"
+	ssh -i "edminforce.pem" ubuntu@ec2-52-10-176-99.us-west-2.compute.amazonaws.com "${CMD}"
 	sudo chmod 777 edminforce.pem;
 }
 
