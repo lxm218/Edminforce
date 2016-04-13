42c96d1eb420f0de9d072ffea9d272624ea07d27
Yifan Wang <ivanrex@gmail.com>
2016-04-12 22:03:12 -0700

    Export Student Report on admin side

--- source/admin-base/client/page/Report/Student.jsx
+++ source/admin-base/client/page/Report/Student.jsx
@@ -114,6 +114,7 @@ KUI.Report_Student = class extends KUI.Page{
 				<Filter ref="filter" />
 				<RC.Div style={{textAlign:'right'}}>
 					<KUI.YesButton onClick={this.search.bind(this)} label="Show Result"></KUI.YesButton>
+					{this.showExportButton()}
 				</RC.Div>
 				<hr/>
 				{this.renderResultTable()}
@@ -206,5 +207,37 @@ KUI.Report_Student = class extends KUI.Page{
 					ref=""></KUI.Table>
 			</RC.Div>
 		);
+	}
+
+	export(){
+		var csvExport = ""
+		for (var key in this.state.result) {
+			csvExport = csvExport + this.exportEachTable(this.state.result[key])
+		}
+		var blob = new Blob([csvExport], {type: "text/plain;charset=utf-8"})
+		saveAs(blob, "StudentReport.csv")
+	}
+
+	exportEachTable(classInfo){
+		var csv = ""
+		if (classInfo.list.length == 0) {
+			return csv
+		}
+		csv = csv + classInfo.classData.nickName + '\n'
+		csv = csv + "Teacher: " + classInfo.classData.teacher + '\n'
+		csv = csv + "Student, Age, Gender, Type\n"
+		for (var i = 0; i < classInfo.list.length; i++) {
+			csv = csv + classInfo.list[i].student.name + ',' +
+				  classInfo.list[i].student.age + ',' + 
+				  classInfo.list[i].student.profile.gender + ',' + 
+				  classInfo.list[i].type + '\n\n'
+		}
+		return csv
+	}
+
+	showExportButton(){
+		if(Object.keys(this.state.result).length != 0) {
+			return (<KUI.YesButton onClick={this.export.bind(this)} style={{marginLeft : '15px'}} label="Export Report" ></KUI.YesButton>);
+		}
 	}
 };
