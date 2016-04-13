0515b1001bc2b58ea05def88d15d5adb7733c20b
Jin Li <jinlie@gmail.com>
2016-04-12 17:32:28 -0700

    JinL - simplify makeup & trial logic to share code

--- source/mobile-base-refactor/server/methods/program.js
+++ source/mobile-base-refactor/server/methods/program.js
@@ -18,43 +18,6 @@ Meteor.methods({
         check([studentID,classID,className], [String]);
         check(lessonDate, Date);
         EdminForce.Registration.bookTrial(this.userId, studentID, classID, className, lessonDate);
-        // console.log(lessonDate);
-        // check([studentID,classID,className], [String]);
-        // check(lessonDate, Date);
-        //
-        // let student = Collections.student.findOne({_id: studentID});
-        // if (!student) {
-        //     throw new Meteor.Error(500, 'Student not found','Invalid student id: ' + studentID);
-        // }
-        //
-        // // update class record
-        // let classData = EdminForce.utils.updateTrialAndMakeupCount('trial', classID, lessonDate);
-        //
-        // if (!classData) {
-        //     throw new Meteor.Error(500, 'No space for trial in the selected class','No space for trial in the selected class: ' + classID);
-        // }
-        //
-        // // insert a class student record
-        // Collections.classStudent.insert({
-        //     accountID: this.userId,
-        //     classID,
-        //     studentID,
-        //     programID: classData.programID,
-        //     lessonDate,
-        //     status: "checkouted",
-        //     type: "trial",
-        //     createTime: new Date()
-        // });
-        //
-        // // Let other method calls from the same client start running,
-        // // without waiting for the email sending to complete.
-        // this.unblock();
-        //
-        // let trialData = {};
-        // trialData[student.name] = {};
-        // trialData[student.name][className] = lessonDate;
-        // let html = EdminForce.utils.getPaymentConfirmEmailTemplate(trialData);
-        // EdminForce.utils.sendEmailHtml(Meteor.user().emails[0].address, 'Trial Class Booking Confirmation',html);
     },
 
     // get a list of trial classes for a program, within specified date range
