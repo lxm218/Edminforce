5fb5f7fe963034d96abfbe6eb457bb30e067d5aa
Jin Li <jinlie@gmail.com>
2016-05-12 09:38:22 -0700

    JinL - add a separate import.sh to data-transfer

---
+++ data-transfer/import.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# export SOURCE=$PWD/../source
+# export IHEALTH_ROOT=$SOURCE/_packages_/ihealth-framework-ui/library
+# export MOBILE_BASE_PACKAGES=$SOURCE/mobile-base0/src/packages
+# export PACKAGE_DIRS=$MOBILE_BASE_PACKAGES:$IHEALTH_ROOT/both:$IHEALTH_ROOT/mobile:$IHEALTH_ROOT/webapp:$PWD/../_packages_/module
+
+# use the collections that after Li Jin Refactor
+export SOURCE=$PWD/../source
+export IHEALTH_ROOT=$SOURCE/_packages_/ihealth-framework-ui/library
+export PACKAGE_DIRS=$IHEALTH_ROOT/both:$IHEALTH_ROOT/mobile:$IHEALTH_ROOT/webapp:$SOURCE/_packages_/module:$SOURCE/mobile-base/packages
+
+
+PORT=4000
+
+# local db
+#export MONGO_URL=mongodb://localhost:27017/EdminForce
+
+# Prod DB
+#export MONGO_URL=mongodb://admin.classforth.com:27017/EdminForce
+
+# Calcolor Stag DB
+#export MONGO_URL=mongodb://calcolor:Icalcolor123!@aws-us-east-1-portal.13.dblayer.com:10587/calcolor-stag?ssl=true
+
+# Calcolor Prod DB
+export MONGO_URL=mongodb://calcolor:Icalcolor123!@aws-us-east-1-portal.13.dblayer.com:10587/calcolor?ssl=true
+
+# Calcolor Demo
+#export MONGO_URL=mongodb://calcolor:Icalcolor123!@aws-us-east-1-portal.13.dblayer.com:10587/demo?ssl=true
+
+#=====================
+#transfer data
+#cd transfer
+#Step 1: install node packages
+#npm install
+#Step 2: start transfer process
+#node index.js
+
+#=====================
+#Update MongoDB
+#cd ../update
+
+cd update
+meteor run --port $PORT
