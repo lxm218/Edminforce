ebab396b194e28ada2f3d452748daff8a90499e1
Jin Li <jinlie@gmail.com>
2016-05-17 10:32:49 -0700

    JinL - update calcolor address

--- source/mobile-base/client/modules/core/components/contact.jsx
+++ source/mobile-base/client/modules/core/components/contact.jsx
@@ -7,7 +7,7 @@ EdminForce.Components.Contact = (props) => (
         <RC.List>
             <RC.ItemIcons uiClass="phone" uiColor="brand2">Phone: 510-580-6883</RC.ItemIcons>
             <RC.ItemIcons uiClass="envelope" uiColor="brand2">Email: help@calcoloracademy.com</RC.ItemIcons>
-            <RC.ItemIcons uiClass="globe" uiColor="brand2">Address: 47816 Warm Springs Blvd Fremont California 94536</RC.ItemIcons>
+            <RC.ItemIcons uiClass="globe" uiColor="brand2">Address: 47816 Warm Springs Blvd, Fremont, California 94536</RC.ItemIcons>
         </RC.List>
     </RC.Div>
 );
