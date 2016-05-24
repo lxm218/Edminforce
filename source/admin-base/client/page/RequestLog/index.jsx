4170916d8dff7b17b0ace6b91e2183e7759f2fae
Jacky <liyang@chinagate.com>
2016-05-24 13:11:09 -0700

    update code

--- source/admin-base/client/page/RequestLog/index.jsx
+++ source/admin-base/client/page/RequestLog/index.jsx
@@ -5,7 +5,9 @@ KUI.RequestLog_Index = class extends KUI.Page{
 		super(p);
 
 		this.state = {
-			page : 1
+			page : 1,
+
+			detail : null
 		};
 	}
 
@@ -29,6 +31,7 @@ KUI.RequestLog_Index = class extends KUI.Page{
 			return util.renderLoading();
 		}
 
+		let self = this;
 		let titleArray = [
 			{
 				title : ' ',
@@ -55,14 +58,28 @@ KUI.RequestLog_Index = class extends KUI.Page{
 				},
 				reactDom(doc){
 
+					let sy = {
+						cursor : 'pointer'
+					};
+
+
 					let show = function(){
-						//TODO
-						alert('comming soon');
+						self.setState({
+							detail : 'loading'
+						});
+						KG.RequestLog.callMeteorMethod('getDetailById', [doc._id], {
+							success : function(rs){
+								self.setState({
+									detail : rs
+								});
+							}
+						});
+						self.refs.modal.show();
 					};
 
 					return (
 						<RC.Div style={{textAlign:'center'}}>
-							<KUI.Icon onClick={show} icon="fa fa-eye" font="18px" color="#1ab394" style={{}}></KUI.Icon>
+							<KUI.Icon onClick={show} icon="fa fa-eye" font="18px" color="#1ab394" style={sy}></KUI.Icon>
 						</RC.Div>
 					);
 				}
@@ -82,6 +99,8 @@ KUI.RequestLog_Index = class extends KUI.Page{
 					list={list}
 					title={titleArray}
 					ref="table"></KUI.PageTable>
+
+				{this.renderDetailDialog()}
 			</RC.Div>
 
 		);
@@ -91,5 +110,77 @@ KUI.RequestLog_Index = class extends KUI.Page{
 		this.setState({
 			page : page
 		});
+	}
+
+	renderDetailDialog(){
+		let param = {
+			title : `Log Detail`,
+			YesFn : function(){
+
+			},
+			renderBody : function(){
+				return (
+					<RC.Div>
+
+						{this.renderDetailBody()}
+					</RC.Div>
+				);
+			}
+		};
+
+		return util.dialog.render.call(this, 'modal', param);
+	}
+
+	renderDetailBody(){
+		let data = this.state.detail;
+
+		if(!data || data === 'loading'){
+			return util.renderLoading();
+		}
+
+		console.log(data);
+
+		let titleArray = [{
+			title : 'Item',
+			key : 'item'
+		}, {
+			title : 'Value',
+			key : 'value'
+		}];
+
+		let list = [
+			{item : 'Operator', value : data.operatorName},
+			{item : 'Client', value : data.client},
+			{item : 'Type', value : data.type}
+		];
+
+		let d = data.result;
+		if(d.customer){
+			list.push({item : 'Customer Name', value : d.customer.name});
+		}
+		if(d.student){
+			list.push({item : 'Student Name', value : d.student.name});
+		}
+		if(d.class){
+			list.push({item : 'Class Name', value : d.class.nickName});
+		}
+
+		if(d.credit){
+			list.push({item : 'Credit', value : d.credit});
+		}
+
+		if(d.note){
+			list.push({item : 'Note', value : d.note});
+		}
+
+
+		return (
+			<KUI.Table
+				style={{}}
+				list={list}
+				title={titleArray}
+				ref="table">
+			</KUI.Table>
+		);
 	}
 };
