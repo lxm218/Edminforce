e008a14103a49231023250dbba502abdc09faacf
Yifan Wang <ivanrex@gmail.com>
2016-05-27 22:55:30 -0700

    a better solution to show policy and term

--- source/mobile-base/client/modules/core/components/login.jsx
+++ source/mobile-base/client/modules/core/components/login.jsx
@@ -39,7 +39,7 @@ EdminForce.Components.User = React.createClass({
     return {
       buttonActive: false,
       waiting: false,
-      action: _.contains(["login","register","reset"], this.props.action) ? this.props.action : "login",
+      action: _.contains(["login","register","reset","policy"], this.props.action) ? this.props.action : "login",
       msg: [],
       notification: null
     }
@@ -147,7 +147,12 @@ EdminForce.Components.User = React.createClass({
   },
 
   showPolicy(){
-    FlowRouter.go('/Policy')
+    this.setState({action: "policy"})
+    //FlowRouter.go('/Policy')
+  },
+
+  showRegister(){
+    this.setState({action: "register"})
   },
 
   startReset(){
@@ -481,6 +486,62 @@ EdminForce.Components.User = React.createClass({
                 {this.state.waiting ? <RC.uiIcon uiClass="circle-o-notch spin-slow" /> : "Send Password Reset E-mail"}
               </RC.Button>
             </RC.Form>
+        )
+        break
+      case "policy":
+        //<div>Reset Password via Email</div>
+        return (
+        <div>
+
+        <h1>Privacy Policy and Terms of Use</h1>
+
+          <h3>Information Collection and Use.</h3>
+          <p>
+          We will not sell, share, or rent this information to any outside parties, except as outlined in this policy. Edminforce Inc may collect information from our users at several different points on our website in order to process your orders and better serve you with pertinent information. Information collected includes name, shipping address, billing address, telephone numbers, e-mail address, and payment information such as credit card. Edminforce Inc may also require you to submit a username and password of your choice for future access to your information. Your username and password is to remain confidential and you should not share this information with anyone.
+          </p>
+
+          <h3>Online Registration. </h3>
+          <p>
+          In order to process your orders using this website, a user must first complete an online sign up form. During the online registration a user is required to give their contact information, which includes name, e-mail, telephone number and physical address. This information is used to contact the user about the services on our site such as sending automated order status updates.</p>
+
+          <h3>Online Order. </h3>
+          <p>
+          Most of the information collected in the online registration process will be used to process your orders. In the order process, a user must provide additional financial information such as credit card number, expiration date, money order, wire transfer or check information. This information is used for billing purposes and to fulfill customer’s orders. If we have trouble processing an order, this contact information is used to get in touch with the user. We do not share your personal and financial information with any third parties. The only exception is when processing your credit card information with the payment gateway and merchant bank for authorization and approval. This process is protected by an enhanced security system SSL and RSA. See Security paragraph for more details on security measures and procedures.</p>
+
+          <h3>Browser Cookies. </h3>
+          <p>
+          With respect to cookies, we use cookies to customize Web page content based on visitors’ browser type or other information that the visitor sends. If a user rejects the cookie, they may still use our site. However you will be unable to use our shopping cart to purchase items online. This is because cookies are used by the shopping cart to retain information necessary to process your order. If you disable cookies, we will not be able to accept your order online. We do not share any cookie information with third party sites. Cookies are used strictly for the purpose of enhancing your personal experience with our site. When you close your browser, the cookies will be terminated.</p>
+
+          <h3>Log Files. </h3>
+          <p>
+          We use IP addresses to analyze trends, administer the site, track user’s movement, and gather broad demographic information for aggregate use. IP addresses are not linked to personally identifiable information. We do not distribute or share IP information with any third parties.</p>
+
+          <h3>Information Disclosure. </h3>
+          <p>
+          We use a credit card processing company to bill users for goods and services, and maybe an outside shipping company (i.e. UPS) to ship orders. These companies do not disclose, retain, share, store or use personally identifiable information for any secondary purposes. Our web site contains links to other sites. Please be aware Edminforce Inc is not responsible for the privacy practices of such other sites. We encourage our users to be aware when they leave our site and to read the privacy statements of each and every web site that collects personally identifiable information. This privacy statement applies solely to information collected by this Web site.</p>
+
+          <h3>Notification of Changes. </h3>
+          <p>
+          If at any point we decide to use personally identifiable information in a manner different from that stated at the time it was collected, we will notify users by way of an email. Users will have a choice as to whether or not we use their information in this different manner. We will use information in accordance with the privacy policy under which the information was collected. If you have opted out of receiving any communications from Edminforce Inc, you may visit our website at www.edminforce.net where news of changes will be posted. Otherwise, you may e-mail us at admin@edminforce.net ask our customers service department for changes and updates. If you send us an e-mail request, we will respond to that e-mail only. You will not receive promotional materials unless you specifically grant us permission to do so.</p>
+
+          <h3>Choice/Opt-out. </h3>
+          <p>
+          Edminforce Inc does not use customer information for contact other than Edminforce Inc pertinent business. Edminforce Inc does not share customer personal information with any outside parties. However, for customers who wish to opt-out or be removed from Edminforce Inc special offers and related news, Edminforce Inc provides several methods to accomplish this request. Users who no longer wish to receive our newsletter or promotional materials may opt-out of receiving these communications by un-checking the permission to send field when processing their order. They may also send us email to admin@edminforce.net with a request to be removed. Finally, they may also use the link provided with all communications to automatically unsubscribe.</p>
+
+          <h3>Information Security. </h3>
+          <p>
+          Edminforce Inc website takes every precaution to protect our user information. When users submit sensitive information via the website, your information is protected both online and off-line. When our registration/order form asks users to enter sensitive information (such as credit card number), the information is encrypted and is protected with the encryption software in the industry – SSL. Our SSL technology is the most advanced 128bit encryption by Authorize.net. To learn more about SSL, follow this link www.Authorize.net. All Edminforce Inc employees are kept up-to-date on our security and privacy practices. Every quarter, as well as any time when new policies are added, our employees are notified and/or reminded about the importance we place on privacy, and what they can do to ensure our customers information is protected. If you have any questions about the security at our website, you can send an email to us at admin@edminforce.net</p>
+
+          <h3>Product/Service Rating and Reviews. </h3>
+          <p>
+          Edminforce Inc may provide a product/service rating feature that allows customers to rate the quality of the product/service. Our review system may also allow for customers to leave written comments to further elaborate their experience with the product/service. All ratings and review comments will be pre-screened by a moderator before posting to the site. The moderator may choose to disregard or eliminate comments that do not meet the guidelines. Guidelines include that all comments are truthful and non-offensive. Guidelines do not permit the posting of personal information or information for the purpose of personal gain.</p>
+
+          <RC.Button onClick={this.showRegister} name="button" theme={buttonTheme}>
+                Back To Register
+          </RC.Button>
+
+
+      </div>
         )
         break
     }
