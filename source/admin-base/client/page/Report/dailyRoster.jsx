ceb081a7a0a9084f30a8a7b7a01de75be01d8c32
Jin Li <jinlie@gmail.com>
2016-06-07 11:01:27 -0700

    JinL - continue on daily roster

--- source/admin-base/client/page/Report/dailyRoster.jsx
+++ source/admin-base/client/page/Report/dailyRoster.jsx
@@ -47,68 +47,91 @@ KUI.Report_DailyRoster = class extends RC.CSS {
         }).bind(this))
     }
 
+    // render daily roster as a HTML table
     renderRoster() {
-        if (!this.data || !this.data.programs || !this.data.programs.length)
-            return null;
+        if (!this.data) return null;
+        if (!this.data.programs || !this.data.programs.length)
+            return (<div>No data available for the selected date</div>);
 
+        // group classes in each program by starting hour
         let hours = [];
         this.data.programs.forEach( (p) => {
             p.classes.forEach( (c) => {
                 c.classTime = moment(c.schedule.time, 'hh:mma');
                 if (hours.indexOf(c.classTime.hours()) < 0)
                     hours.push(c.classTime.hours());
-                c.classTime = c.classTime.toDate();
             })
         });
-
         hours.sort();
-        
+
+        // calculate column width, the first column is smaller, the rest of the columns are equally sized
         let colWidth = Math.floor(100 / this.data.programs.length);
         let timeColWidth = 100 - colWidth * this.data.programs.length;
         while (timeColWidth + this.data.programs.length < colWidth - 1) {
             colWidth--;
             timeColWidth+=this.data.programs.length;
         }
+
+        // create table column width elements
         let colWidthElements = new Array(this.data.programs.length);
         colWidthElements.fill(<col width={colWidth + "%"} />);
         colWidthElements.unshift(<col width={timeColWidth + "%"} />);
 
+        // create table rows
         let rows = [];
+        // currentHour stores rows from each program for the current hour
         let currentHour = new Array[this.data.programs.length];
         currentHour.fill({});
         hours.forEach( (hour) => {
             // max number of rows in all program columns
             let maxRowCount = 0;
 
-            // iterate through all programs
+            // iterate through all programs, generate table columns for all classes in each program
             this.data.programs.forEach( (p, index) => {
                 // classes from each program, start in the current hour
-                currentHour[index].classes = _.filter(p.classes, (c) => c.classTime.getHours() == hour);
+                let currentHourClasses = _.filter(p.classes, (c) => c.classTime.hours() == hour);
 
                 // generate rows for all classes, and teachers
                 currentHour[index].rows = [];
                 // sort classes by start time
-                if (currentHour[index].classes.length > 0) {
-                    currentHour[index].classes.sort( (a,b) => a.classTime.getTime() - b.classTime.getTime());
-                    currentHour[index].classes.forEach( (c) => {
-                        currentHour[index].rows.push({"teacher":c.teacher});
+                if (currentHourClasses.length > 0) {
+                    currentHourClasses.sort( (a,b) => a.classTime.valueOf() - b.classTime.valueOf());
+                    currentHourClasses.forEach( (c) => {
+                        currentHour[index].rows.push({"teacher": c.classTime.format("hh:mm ") + c.teacher});
                         currentHour[index].rows.concat(c.students);
                     })
                 }
 
-                if (maxRowCount < currentHour[index].rows.length) maxRowCount = currentHour[index].rows.length;
+                if (maxRowCount < currentHour[index].rows.length)
+                    maxRowCount = currentHour[index].rows.length;
             });
 
+            // generate table rows
             for (let iRow = 0; iRow < maxRowCount; iRow++) {
                 let tdElements = [];
                 iRow == 0 && (tdElements.push( <td rowSpan={maxRowCount}>{moment().hours(hour).format("hh a")}</td> ))
 
                 currentHour.forEach( (p) => {
-                    if (p.rows.length > iRow) {
-                        tdElements.push(<td>{p.rows[iRow].teacher || p.rows[iRow].name}</td>);
+                    if (iRow < p.rows.length) {
+                        // class name or student name
+                        if (p.rows[iRow].teacher) {
+                            // show class time and teacher in a "th" style
+                            tdElements.push(<th>{p.rows[iRow].teacher}</th>);
+                        }
+                        else {
+                            let tdContent = p.rows[iRow].name;
+                            if (p.rows[iRow].type == 'trial')
+                                tdContent += ' (trial)';
+                            else
+                            if (p.rows[iRow].type == 'makeup')
+                                tdContent += ' (make up)';
+
+                            tdElements.push(<td>{tdContent}</td>);
+                        }
                     }
                     else
-                    if (p.rows.length == iRow && iRow < maxRowCount-1) {
+                    if (iRow == p.rows.length && iRow < maxRowCount-1) {
+                        // merged cell to show empty space
                         tdElements.push(<td rowSpan={maxRowCount - iRow}></td>);
                     }
                 })
