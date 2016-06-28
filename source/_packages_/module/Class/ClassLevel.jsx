4e6e347504dd2fe3e5e220548b02f581462f715b
Jacky <liyang@chinagate.com>
2016-06-28 15:16:19 -0700

    rename file

---
+++ source/_packages_/module/Class/ClassLevel.jsx
@@ -0,0 +1,33 @@
+let Base = KG.getClass('Base');
+KG.define('EF-ClassLevel', class extends Base{
+    defineDBSchema(){
+        return Schema.ClassLevel;
+    }
+
+
+    defineMeteorMethod(){
+        let self = this;
+
+        return {
+            getAllByQuery : function(query={}, option={}){
+                return self._db.find(query, {
+                    sort : option.sort || {
+                        createTime : -1
+                    }
+                }).fetch();
+            },
+
+            removeById : function(id){
+                let m = KG.DataHelper.getDepModule();
+                let x = m.Class.getDB().find({
+                    levels : {$in : [id]}
+                }).count();
+
+                if(x.length > 0) return false;
+
+                self._db.remove({_id : id});
+                return true;
+            }
+        };
+    }
+});
