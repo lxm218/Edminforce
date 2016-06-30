525c1891b8953087adbcc0aad2229ceaf62b651d
Jacky <liyang@chinagate.com>
2016-06-30 11:42:52 -0700

    update code

--- source/admin-base/client/page/Classes/detail.jsx
+++ source/admin-base/client/page/Classes/detail.jsx
@@ -97,7 +97,7 @@ KUI.Class_detail = class extends KUI.Page{
         let rs = KG.get('EF-Class').updateById(data, this.getClassId());
         KG.result.handle(rs, {
             success : function(json){
-
+console.log(arguments)
                 util.toast.alert('update success');
                 util.goPath('/program/class');
 
