f32ce474fb1f21ed9ff466178f1672c230f3d09c
Jin Li <jinlie@gmail.com>
2016-07-08 06:57:26 -0700

    JinL - issue#550 students page reformat

--- source/mobile-base/client/modules/students/components/students.jsx
+++ source/mobile-base/client/modules/students/components/students.jsx
@@ -1,5 +1,7 @@
 let _ = lodash;
 let {
+    Tabs,
+    Tab,
     Table,
     TableHeaderColumn,
     TableRow,
@@ -8,113 +10,151 @@ let {
     TableBody
 }=MUI;
 
+function getClassName(cls) {
+    return `${cls.program.name} ${cls.class.schedule.day} ${cls.class.schedule.time} ` + (cls.type == 'makeup' || cls.type=='trial' ? moment(cls.lessonDate).format("MMM D, YYYY") : '');
+}
+function getClassLink(cls, studentID) {
+    let params = {
+        studentID,
+    };
+    let query = {
+        current: cls._id,
+    }
+    return FlowRouter.path("/studentClass/:studentID", params, query);
+}
+
+const StudentClassTable = (props) => (
+    <Table selectable={false}>
+        <TableHeader adjustForCheckbox={false} displaySelectAll={false} enableSelectAll={false}>
+            <TableRow>
+                <TableHeaderColumn>Category</TableHeaderColumn>
+                <TableHeaderColumn>Class</TableHeaderColumn>
+            </TableRow>
+        </TableHeader>
+
+        <TableBody displayRowCheckbox={false}>
+            {
+                props.categories.map( (c) => (
+                    <TableRow key={c.category}>
+                        <TableRowColumn style={{whiteSpace:"normal"}}>{c.category}</TableRowColumn>
+                        <TableRowColumn style={{whiteSpace:"normal"}}>
+                            {
+                                c.classes.map( (cls,index) => (
+                                    <p style={{paddingTop: index == 0 ? 6 : 0, paddingBottom: 6}}  key={cls._id}>
+                                        {
+                                            cls.current ? (<a href={getClassLink(cls, props.student)}>{getClassName(cls)}</a>) : getClassName(cls)
+                                        }
+                                    </p>
+                                ))
+                            }
+                        </TableRowColumn>
+                    </TableRow>
+                ))
+            }
+        </TableBody>
+    </Table>
+)
+
+
 EdminForce.Components.Students = class extends RC.CSS {
 
     constructor(p) {
         super(p);
-    }
 
-    selectClass(student, studentClass) {
-        // it has class
-        if (student && studentClass && studentClass.type !== 'trial' && studentClass.type !== 'makeup' ) {
+        // for RC.Select
+        _.forEach(this.props.students, (c) => {
+            c.value = c._id;
+            c.label = c.name;
+        })
 
-            let params = {
-                studentID: student._id
-            };
-            let query = {
-                current: studentClass._id,
-            }
-            let path = FlowRouter.path("/studentClass/:studentID", params, query);
-            FlowRouter.go(path);
+        this.state = {
+            selectedStudent: this.props.students.length > 0 ? this.props.students[0]._id : null
         }
+
+        this.onStudentChanged = this.onStudentChanged.bind(this);
     }
 
-    selectStudent(student) {
-        // update student
-        if (!student) return;
+    onStudentChanged(event) {
+        this.setState({
+            selectedStudent: event.target.value
+        })
+    }
 
-        let path = FlowRouter.path("/student/:studentID", {studentID: student._id}, {r:'/students'});
-        FlowRouter.go(path);
+    groupClassByCategory(classes) {
+        let trials = [];
+        let makeups = [];
+        let sessions = [];
+        classes.forEach( (c) => {
+            if (c.type == 'makeup')
+                makeups.push(c);
+            else
+            if (c.type == 'trial')
+                trials.push(c);
+            else {
+                let session = _.find(sessions, {category: c.session.name});
+                if (!session) {
+                    session = {
+                        category: c.session.name,
+                        startDate: c.session.startDate,
+                        classes: []
+                    }
+                    sessions.push(session);
+                }
+                session.classes.push(c);
+            }
+        })
+        // sort session by startDate in ascendaing order
+        sessions.sort( (a,b) => (a.startDate.getTime() - b.startDate.getTime()));
+        if (trials.length > 0) {
+            sessions.push({
+                category: 'Trial Class',
+                classes: trials
+            })
+        }
+        if (makeups.length > 0) {
+            sessions.push({
+                category: 'Make up Class',
+                classes: makeups
+            })
+        }
+
+        return sessions;
     }
 
     render() {
-
-        let self = this;
-
-        let currentTime = new Date().getTime();
-        let studentElements = this.props.students.map((student) => {
-
-            // show all current classes, and one most recent completed class, if any.
-            let classes = student.currentClasses;
-            if (student.completedClasses.length > 0) {
-                student.completedClasses[0].completed = true;
-                classes = student.currentClasses.concat([student.completedClasses[0]]);
+        // group student class by category
+        let currentClassesByCategory = [];
+        let historyClassesByCategory = [];
+        if (this.state.selectedStudent) {
+            let student = _.find(this.props.students, {_id:this.state.selectedStudent});
+            if (student) {
+                student.currentClasses.forEach( (c) => {
+                    c.current = (c.type == 'register');
+                });
+                currentClassesByCategory = this.groupClassByCategory(student.currentClasses);
+                historyClassesByCategory = this.groupClassByCategory(student.completedClasses);
             }
-
-            // class records for this student
-            let classElements = classes.map((sc, index) => (
-                <RC.Div key={student.id + sc._id} onClick={self.selectClass.bind(self, student,sc)}>
-                    <p style={{padding: 0, paddingTop: index == 0 ? 8 : 0}}>
-                        {sc.program.name}
-                    </p>
-                    <p style={{padding: 0}}>
-                        {sc.session.name} {sc.class.schedule && sc.class.schedule.day} {sc.class.schedule && sc.class.schedule.time}
-                    </p>
-                    {
-                        !sc.completed && (
-                            <p style={{padding: 0}}>
-                                {
-                                    (sc.type === "trial" || sc.type==='makeup') ? (sc.type === "trial" ? "Trial on " : "Make up on ") + moment(sc.lessonDate).format("MMM D, YYYY") : (sc.session.startDate.getTime() < currentTime ? "Current" : "New Registration")
-                                }
-                            </p>
-                        )
-                    }
-                    <p style={{padding: 0, paddingBottom: 8 }}>
-                        {sc.completed ? "Completed" : (sc.createTime && "Registered on " + moment(sc.createTime).format("MMM D, YYYY"))}
-                    </p>
-                </RC.Div>
-            ))
-
-            if (classElements.length === 0) {
-                classElements.push(
-                    <RC.Div key={student._id+"noclass"}>
-                        <p style={{padding: 0, paddingBottom: 8, paddingTop: 8}}>No class registration record</p>
-                    </RC.Div>
-                )
-            }
-
-            return (
-                <TableRow key={student._id}>
-                    <TableRowColumn style={{width: "40%", whiteSpace:"normal"}}>
-                        <p onClick={self.selectStudent.bind(self, student)}>
-                            {student.name}
-                        </p>
-                    </TableRowColumn>
-                    <TableRowColumn style={{width: "60%", whiteSpace:"normal"}}>
-                        {classElements}
-                    </TableRowColumn>
-                </TableRow>
-            )
-        })
+        }
 
         return (
             <RC.Div>
                 <RC.Div style={{borderBottom:"1px solid #e0e0e0", paddingBottom:8}}>
                     <h3 style={{"textAlign": "center"}}>Booking Records</h3></RC.Div>
-                    <RC.VerticalAlign center={true} className="padding"><h6>Click current class to book makeup class</h6></RC.VerticalAlign>
                     {EdminForce.utils.renderError(this.props.error)}
-                    <Table selectable={false} onRowSelection={self.selectStudent}>
-                        <TableHeader displaySelectAll={false} enableSelectAll={false} style={{display:"none"}}>
-                            <TableRow>
-                                <TableHeaderColumn>Student</TableHeaderColumn>
-                                <TableHeaderColumn>Class</TableHeaderColumn>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody displayRowCheckbox={false} onRowSelection={self.selectStudent}>
-                            {studentElements}
-                        </TableBody>
-                    </Table>
-
+                    <RC.Select options={this.props.students}
+                               style={{borderBottom:'none'}}
+                               value={this.state.selectedStudent}
+                               label="Select Student"
+                               labelColor="brand1"
+                               onChange={this.onStudentChanged}/>
+                    <Tabs>
+                        <Tab key="current" label="Current or upcoming classes" value="current">
+                            <StudentClassTable categories={currentClassesByCategory} student={this.state.selectedStudent}></StudentClassTable>
+                        </Tab>
+                        <Tab key="history" label="History classes" value="history">
+                            <StudentClassTable categories={historyClassesByCategory} student={this.state.selectedStudent}></StudentClassTable>
+                        </Tab>
+                    </Tabs>
             </RC.Div>
         );
     }
