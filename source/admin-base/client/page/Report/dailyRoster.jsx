b2d394f05fec328d8859dfba56409a81243c1b63
Jeffrey Fan <ennea8@gmail.com>
2016-09-13 23:01:54 +0800

    1.Add css rule reserve background color for print view 2.Beautify print format

--- source/admin-base/client/page/Report/dailyRoster.jsx
+++ source/admin-base/client/page/Report/dailyRoster.jsx
@@ -153,7 +153,7 @@ KUI.Report_DailyRoster = class extends RC.CSS {
                         // class name or student name
                         if (p.rows[iRow].teacher) {
                             // show class time and teacher in a "th" style
-                            tdElements.push(<th key={"c"+hour+"_" + iRow + "_" + index} style={{textAlign:"center",background:programPalette[index % programPalette.length]}}>
+                            tdElements.push(<th key={"c"+hour+"_" + iRow + "_" + index} style={{textAlign:"center", background:programPalette[index % programPalette.length] + ' !important'}}>
                                 <a href="/teachers">{p.rows[iRow].teacher}</a>
                             </th>);
                         }
@@ -185,7 +185,7 @@ KUI.Report_DailyRoster = class extends RC.CSS {
 
         let titleStyles = [{
             textAlign:"center",
-            background: programTitleColor
+            background: programTitleColor + ' !important'
         }, {
             textAlign:"center"
         }]
@@ -222,13 +222,13 @@ KUI.Report_DailyRoster = class extends RC.CSS {
         return (
             <RC.Div>
                 <h3 style={{"textAlign": "left"}} className="print-hidden" >Daily Roster</h3>
-                <RB.Row>
+                <RB.Row className="print-hidden">
                     <div className="form-horizontal">
                         <RB.Col md={6} mdOffset={0}>
                             <RB.Input type="text" {... p.date} value={moment(this.state.selectedDate).format("MM/DD/YYYY")} onChange={this.onDateEdit}>
                             </RB.Input>
                         </RB.Col>
-                        <RB.Col  md={6} mdOffset={0} className="print-hidden">
+                        <RB.Col  md={6} mdOffset={0} >
                             <KUI.YesButton onClick={this.getDailyRoster} label="Show"></KUI.YesButton>
                         </RB.Col>
                     </div>
@@ -240,6 +240,7 @@ KUI.Report_DailyRoster = class extends RC.CSS {
                             <RB.Button bsStyle="link"><RB.Glyphicon glyph="print" /></RB.Button>
                         </a>
                     }
+                    <h2 className="print-show" style={{textAlign:'center'}}> Daily Roster {moment(this.state.selectedDate).format("MM/DD/YYYY")} </h2>
                     {this.renderRoster()}
                 </RC.Div>
             </RC.Div>
