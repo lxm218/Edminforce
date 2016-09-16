a96839077c94058c931eae402b2a20ed87a5f063
Jin Li <jinlie@gmail.com>
2016-09-16 15:02:45 -0700

    JinL - create a script to generate non-returning student report

--- source/mobile-base/server/main.js
+++ source/mobile-base/server/main.js
@@ -12,6 +12,65 @@
 //     fs.writeFile( "test.html", email, (err)=>{console.log(err)}) ;
 // }
 
+/**
+ * Get all students of a session
+ * @param sessionName
+ * @returns {Array}
+ */
+function getStudentsBySession(sessionName) {
+    let session = Collections.session.findOne({name:sessionName});
+    if (!session) return [];
+
+    // get all classes for this session
+    let classes = Collections.class.find({sessionID:session._id}, {level:1}).fetch();
+    let students = [];
+    classes.forEach( cls => {
+        let classStudents = Collections.classStudent.find({classID:cls._id}, {fields:{studentID:1, accountID:1}}).fetch();
+        let studentIDs = classStudents.map( classStudent => classStudent.studentID );
+        classStudents = Collections.student.find({_id: {$in: studentIDs}}, {fields:{name:1,accountID:1}}).fetch();
+        classStudents.forEach( clsStudent => {
+            if (_.find(students, {_id:clsStudent._id})) return;
+
+            let customer = Collections.Customer.findOne({_id:clsStudent.accountID});
+            if (customer) {
+                clsStudent.phone = customer.phone;
+                clsStudent.email = customer.email;
+                students.push(clsStudent);
+            }
+            else {
+                console.log(`Customer ${clsStudent.accountID} not found, studentID: ${clsStudent._id}`);
+            }
+
+            console.log(clsStudent);
+        });
+    });
+
+    return students;
+}
+
+/**
+ * Generate a report that shows all non-returning students between two sessions
+ */
+function generateReport() {
+    let fs = Npm.require( 'fs' );
+
+    // get all students for a session
+    let springStudents = getStudentsBySession('Spring 2016');
+    let fallStudents = getStudentsBySession('Fall 2016');
+
+    let nonReturningStudents = [];
+    springStudents.forEach( student => {
+        if (!_.find(fallStudents, {_id: student._id})) {
+            nonReturningStudents.push(student);
+            console.log(student);
+        }
+    })
+
+    var buffer = new Buffer(JSON.stringify(nonReturningStudents)) ;
+    fs.writeFileSync( "report.json", buffer ) ;
+    console.log('Done');
+}
+
 Meteor.startup( () => {
     //EdminForce.Registration.syncClassRegistrationCount();
 })
